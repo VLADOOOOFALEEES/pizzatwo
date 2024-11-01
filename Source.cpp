a7c5fd3970a61d19567253dffd737d57ce55777a
ElNefro <Yanik0206@gmail.com>
2024-11-01 15:30:41 +0500

    dedline

--- Source.cpp
+++ Source.cpp
@@ -5,15 +5,23 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	double pizza;
-	double pizza1;
-	double zakuski;
-	double zakuski1;
-	double napitki;
-	double napitki1;
-	int choice;
-	double result;
+	int vibor{};
+	double pizza{};
+	double pizza1{};
+	double zakuski{};
+	double zakuski1{};
+	double napitki{};
+	double napitok{};
+	double kolvpizza{};
+	double kolvzakuski{};
+	double kolvnapitki{};
+	int choice{};
+	double result{};
+	double resultpizza{};
+	double resultzakuski{};
+	double resultnapitok{};
 	double answer = 1;
+	double answer1 = 1;
 	while (answer == 1)
 	{
 		for (int i = 0; i < 20; i++)
@@ -25,74 +33,133 @@ int main()
 		std::cout << "Добро пожаловать в пиццерию Владика!\n";
 		std::cout << "1.Пицца  2.Закуски  3.Напитки  ";
 		std::cin >> choice;
-			std::cout << "1.Гавайская\n2.Пепрони\n3.Классическая\n4.Маленькие Буки\n5.Мясная  ";
-			std::cin >> pizza;
-			if (pizza == 1)
+		if (choice == 1)
+		{
+			while (true)
 			{
-				pizza1 = 90;
+				if (pizza == 6)
+				{
+					break;
+				}
+				else
+				{
+
+					std::cout << "1.Гавайская\n2.Пепрони\n3.Классическая\n4.Маленькие Буки\n5.Мясная\n6.Дальше  ";
+					std::cin >> pizza;
+					if (pizza == 1)
+					{
+						pizza1 = 90;
+					}
+					if (pizza == 2)
+					{
+						pizza1 = 60;
+					}
+					if (pizza == 3)
+					{
+						pizza1 = 50;
+					}
+					if (pizza == 4)
+					{
+						pizza1 = 100;
+					}
+					if (pizza == 5)
+					{
+						pizza1 = 70;
+					}
+					if (pizza != 6)
+					{
+						std::cout << "Сколько? ";
+						std::cin >> kolvpizza;
+					}
+					resultpizza = kolvpizza * pizza1;
+				}
 			}
-			if (pizza == 2)
+		}
+		while (true)
+		{
+			if (zakuski == 4)
 			{
-				pizza1 = 60;
+				break;
 			}
-			if (pizza == 3)
+			else
 			{
-				pizza1 = 50;
+				std::cout << "1.Чипсеки\n2.Нагетсы\n3.Картофка\n4.Дальше ";
+				std::cin >> zakuski;
+				if (zakuski == 1)
+				{
+					zakuski1 = 80;
+				}
+				if (zakuski == 2)
+				{
+					zakuski1 = 120;
+				}
+				if (zakuski == 3)
+				{
+					zakuski1 = 100;
+				}
+				if (zakuski != 4)
+				{
+					std::cout << "Сколько? ";
+					std::cin >> kolvzakuski;
+				}
+				resultzakuski = kolvzakuski * zakuski1;
 			}
-			if (pizza == 4)
+		}
+		while (true)
+		{
+			if (napitki == 5)
 			{
-				pizza1 = 100;
+				break;
 			}
-			if (pizza == 5)
+			else {
+				std::cout << "1.Кола\n2.Спрайт\n3.Фантата\n4.Чай\n5.Дальше ";
+				std::cin >> napitki;
+				if (napitki == 1)
+				{
+					napitok = 70;
+				}
+				if (napitki == 1)
+				{
+					napitok = 60;
+				}
+				if (napitki == 3)
+				{
+					napitok = 60;
+				}
+				if (napitki == 4)
+				{
+					napitok = 20;
+				}
+				if (napitki != 5)
+				{
+					std::cout << "Сколько? ";
+					std::cin >> kolvnapitki;
+				}
+				resultnapitok = kolvnapitki * napitok;
+			}
+		}
+			result = resultpizza + resultzakuski + resultnapitok;
+			std::cout << "Всё обошлось вам в " << result << " рублей";
+			if (result >= 1500)
 			{
-				pizza1 = 70;
+				std::cout << "\nВаша покупка составила более чем 1500р мы делаем вам скидку в 15%!\n";
+				result = result * 85 / 100;
+				std::cout << "\nВсё обошлось вам в " << result << " рублей";
 			}
-		
-		
-		
-			std::cout << "1.Чипсеки\n2.Нагетсы\n3.Картофка  ";
-			std::cin >> zakuski;
-			if (zakuski == 1)
-			{
-				zakuski1 = 80;
-			}
-			if (zakuski == 2)
-			{
-				zakuski1 = 120;
-			}
-			if (zakuski == 3)
-			{
-				zakuski1 = 100;
-			}
-			
-		
-			std::cout << "1.Кола\n2.Спрайт\n3.Фантата\n4.Чай  ";
-			std::cin >> napitki;
-			if (napitki == 1)
-			{
-				napitki1 = 70;
-			}
-			if (napitki == 1)
-			{
-				napitki1 = 60;
-			}
-			if (napitki == 3)
-			{
-				napitki1 = 60;
-			}
-			if (napitki == 4)
-			{
-				napitki1 = 20;
-			}
-		
-		
+		if (kolvpizza == 4)
+		{
+			std::cout << "\nВы заказали 4 кусочка пиццы, мы дарим вам 5!";
+		}
+
+		if (kolvnapitki > 4)
+		{
+			std::cout << "\nОдин напиток мы отдаём вам бесплатно!";
+		}
 		std::cout << "\nВернуться в меню? 1 - да";
 		if (answer != 0)
 		{
 			std::cin >> answer;
 		}
-		result = pizza1 + zakuski1 + napitki1;
-		std::cout << "Всё обошлось вам в" << result << "рублей";
 	}
 
 
